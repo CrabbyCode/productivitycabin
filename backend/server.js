2f82866dbc9cc8e3f02474838c572cc10b8bffd7
vxZeroKun <mmmakki9@gmail.com>
2024-05-12 19:58:50 +0300

    login

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,10 @@ app.get("/projects", function (req, res) {
   }
 });
 
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
 app.listen(3000, function () {
   console.log("listening on port 3000..");
 
